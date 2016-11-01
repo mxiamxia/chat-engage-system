bdf01621735f79fe1ab31a5156fd8fa8f1150f17
mxia <mxiamxia@gmail.com>
2016-10-31 21:40:39 -0400

    update

--- core/prologCm.js
+++ core/prologCm.js
@@ -25,10 +25,10 @@ var processPrologMessage = function(id, message, robot, app, room) {
         prop = JSON.parse(prop);
     }
 
-    if (_.isEmpty(text)) {
-        logger.debug('the input text is empty');
-        return;
-    }
+    // if (_.isEmpty(text)) {
+    //     logger.debug('the input text is empty');
+    //     return;
+    // }
 
     var ep = new EventProxy();
     if (typeof room === 'undefined' || room === null) {
